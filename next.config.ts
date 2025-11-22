ef9682d15181682f50657226d7e0b853eb71bc90
James Thomas <codeactor@protonmail.com>
2025-11-22 11:20:10 +0000

    Improve configs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  poweredByHeader: false,
+  reactStrictMode: true,
 };
 
 export default nextConfig;
